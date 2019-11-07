851dd0f6b435d8d7222620a280e28abea8a200fd
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2019-11-07 07:43:02 +0000

    Strings in ES6/ES2015 , Arrow Fuctions:Basics Arrow Fuctions:Lexical 'this' Keyword(45%)

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -88,6 +88,96 @@ console.log(i);
 console.log(c);
 */
 
-/* Strings in ES6/ES2015 --------------------------------------------*/
+/* Strings in ES6/ES2015 -------------------------------------------*/
 /*------------------------------------------------------------------*/
 /*------------------------------------------------------------------*/
+
+/*
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2016 - year;
+}
+
+// ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' year old.');
+
+// ES6 Template literals
+console.log(`This is ${firstName} ${lastName}. He was borrn in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+console.log(n.startsWith('J')); // true
+console.log(n.endsWith('th')); // true
+console.log(n.includes('oh')); // true
+console.log(firstName.repeat(5)); //JohnJohnJohnJohnJohn
+console.log(`${firstName} `.repeat(5)); //John John John John John
+*/
+
+/* Arrow Fuctions:Basics -------------------------------------------*/
+/*------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
+
+/*
+const years = [1990, 1965, 1982, 1937];
+
+// ES5
+var ages5 = years.map(function (el) {
+    return 2016 - el;
+});
+console.log(ages5);
+
+
+// ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+console.log(ages6);
+*/
+
+/* Arrow Fuctions:Lexical 'this' Keyword ---------------------------*/
+/*------------------------------------------------------------------*/
+/*------------------------------------------------------------------*/
+
+// ES5
+var box5 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+
+        var self = this;
+        document.querySelector('.green').addEventListener('click', function () {
+            // We made the self variable beacuse This function call does not have access to the "this" keyword
+            // beacuse we can only have access to it in the method not a function within a method
+            var str = 'This is box number ' + self.position + ' and it is ' + self.color;
+            alert(str);
+        });
+    }
+}
+//box5.clickMe();
+
+
+// ES6
+const box6 = {
+    color: 'green',
+    position: 1,
+    clickMe: function () {
+        document.querySelector('.green').addEventListener('click', () => {
+
+            // We now have a function that shares the this keyword with its surrounding
+            // so we can use the this keyword from the clickme method
+            const str = 'This is box number ' + this.position + ' and it is ' + this.color;
+            alert(str);
+        });
+    }
+}
+box6.clickMe();
