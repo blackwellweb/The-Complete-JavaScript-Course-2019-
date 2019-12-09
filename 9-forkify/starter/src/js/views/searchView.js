cd1aea8eafc60d60f8ab6138b08c98a28bb34e11
blackwellweb <P.Blackwell1@uni.brighton.ac.uk>
2019-12-09 07:17:10 +0000

    Building the Recipe View - Part 2 (100%)

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -15,6 +15,16 @@ export const clearResults = () => {
     elements.serachResPages.innerHTML = '';
 };
 
+
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 /*
 // NOTE: How imitRecipeTitle() works
 // 'Pasta with tomato and spinach'
